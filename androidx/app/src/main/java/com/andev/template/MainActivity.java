d24c40aca6a04341288520cb673fd9ea24fd8d5d
Hax4Us <lkpandey950@gmail.com>
2021-07-11 11:49:56 +0530

    initial commit

---
+++ androidx/app/src/main/java/com/andev/template/MainActivity.java
@@ -0,0 +1,15 @@
+package com.andev.template;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
